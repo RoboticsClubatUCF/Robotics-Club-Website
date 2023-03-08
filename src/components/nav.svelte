2d56a3e690f7c10555c16f279bce87d1c08842dc
BiggiePete <peter.cross222@gmail.com>
2023-03-07 19:49:20 -0500

    init

---
+++ src/components/nav.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { info } from "../data/info";
+
+
+</script>
+
+
+<div>
+    <h1>{{info.title}}</h1>
+</div>
+
+<style></style>
---
+++ src/data/info.ts
@@ -0,0 +1,10 @@
+/*
+This file contains hard coded information for the site, including title, and colors
+*/
+
+export const info={
+    title:"Robotics Club of Central Florida",
+    
+}
+
+
