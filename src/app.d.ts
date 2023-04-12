c8080414ac30628e7af89729ead78a475e1b6bfa
BiggiePete <peter.cross222@gmail.com>
2023-04-11 20:21:50 -0400

    implements lucia 1

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,15 +1,19 @@
-import { PrismaClient } from '@prisma/client';
-
-// src/app.d.ts
+// app.d.ts
 /// <reference types="lucia-auth" />
-declare namespace Lucia {
-	type Auth = import('$lib/server/lucia').Auth;
-	type UserAttributes = {};
-}
 
+import type { PrismaClient } from '@prisma/client';
+
+// app.d.ts
 /// <reference types="@sveltejs/kit" />
 declare namespace App {
-	interface Locals {
-		auth: import('lucia-auth').AuthRequest;
+	type AuthRequest = import("lucia-auth").AuthRequest;
+	// Locals must be an interface and not a type
+	// eslint-disable-next-line @typescript-eslint/no-empty-interface
+	interface Locals extends AuthRequest {}
+}
+
+declare global {
+	namespace App {
+		let prisma: PrismaClient;
 	}
 }
