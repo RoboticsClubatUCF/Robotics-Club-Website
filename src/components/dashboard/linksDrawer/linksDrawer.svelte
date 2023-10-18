8bf725e0d85298bca3fc62d49a639ae5ef4f549f
BiggiePete <pter.cross222@gmail.com>
2023-10-18 18:57:17 -0400

    new links

--- src/components/dashboard/linksDrawer/linksDrawer.svelte
+++ src/components/dashboard/linksDrawer/linksDrawer.svelte
@@ -1,9 +1,24 @@
 <script lang="ts">
   import type { Role } from '@prisma/client';
+  import config from '../../../config';
 
   let role: Role;
 </script>
 
 <div class="p-2 m-2">
   <h3 class="h3">Links to other Dashboards</h3>
+  {#if role.name == config.roles.officer.name}
+    <!-- user is an officer, of what type, we do not know -->
+
+    <!-- make availaible to every officer the following -->
+    <a href="/admin" class="btn variant-ghost-tertiary hover:variant-filled-tertiary">Admin</a>
+
+    <a href="/lead" class="btn variant-ghost-tertiary hover:variant-filled-tertiary">Teamlead</a>
+  {/if}
+  {#if role.name == config.roles.lead.name}
+    <!-- user is an officer, of what type, we do not know -->
+
+    <!-- make availaible to every officer the following -->
+    <a href="/lead" class="btn variant-ghost-tertiary hover:variant-filled-tertiary">Teamlead</a>
+  {/if}
 </div>
