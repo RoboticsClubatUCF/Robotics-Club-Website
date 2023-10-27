faa9c01de5b6281ba3521e045c47181e38990253
BiggiePete <pter.cross222@gmail.com>
2023-10-26 23:20:18 -0400

    beginning individual project pages

---
+++ src/routes/(main)/member/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import type { PageData } from './$types';
+
+  export let data: PageData;
+</script>
+
+<div class="card p-4 m-4">
+  <h2 class="h2">HEY! how did you get here!</h2>
+  <a class="btn variant-filled-secondary" href="/">Go Back</a>
+</div>
---
+++ src/routes/(main)/member/[slug]/+page.server.ts
@@ -0,0 +1,5 @@
+import type { PageServerLoad } from './$types';
+
+export const load = (async ({ params }) => {
+  return {};
+}) satisfies PageServerLoad;
