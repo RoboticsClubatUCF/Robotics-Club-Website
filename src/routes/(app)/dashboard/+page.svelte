9e797def03771fc00e1aab433c73c0346aa0a780
BiggiePete <pter.cross222@gmail.com>
2024-02-18 15:23:17 -0500

    clean up old comments

--- src/routes/(app)/dashboard/+page.svelte
+++ src/routes/(app)/dashboard/+page.svelte
@@ -8,25 +8,14 @@
   } from '@skeletonlabs/skeleton';
   import type { PageServerData } from './$types';
   import { superForm } from 'sveltekit-superforms/client';
-  import { onMount } from 'svelte';
   import Feed from '../../../components/dashboard/feed.svelte';
   import LeftSideBar from '../../../components/dashboard/leftSidebar/leftSideBar.svelte';
   import RightSideBar from '../../../components/dashboard/rightSidebar/rightSideBar.svelte';
   import Payments from '../../../components/stripe/payments.svelte';
   export let data: PageServerData;
-  let email = data.user!.email;
-
-  let mounted = false;
   const { form, errors, constraints } = superForm(data.form, {
     clearOnSubmit: 'errors-and-message'
   });
-  onMount(() => {
-    mounted = true;
-  });
-  let paymentSuccess = {
-    success: false,
-    duesType: 1
-  };
   const drawerStore = getDrawerStore();
   const drawerSettingsLeft: DrawerSettings = {
     id: 'dashboard1',
@@ -119,63 +108,6 @@
     {#if !((data.user?.membershipExpDate.getTime() ?? 0) < new Date().getTime())}
       <br />
       <!-- <SumoBotsSignUp /> -->
-      <!-- <div
-        class={$modeCurrent
-          ? 'block card p-8 pointer-events-auto shadow-xl shadow-surface-300 sm:w-screen md:w-5/6 justify-center card-hover'
-          : 'block card p-8 pointer-events-auto shadow-xl shadow-surface-500 sm:w-screen md:w-5/6 justify-center card-hover'}
-      >
-        <div class="p-2 rounded-md">
-          <h2 class="h2">Test Planka</h2>
-          <br />
-          <div>
-          </div>
-          <button
-            class="btn variant-ghost-secondary hover:variant-filled-secondary"
-            on:click={callPlanka}>Register</button
-          >
-        </div>
-      </div> -->
     {/if}
   </div>
 </AppShell>
-
-<!-- This entire page needs to be re-done to allow for the new dashboard -->
-<!-- <div
-  class="h-screen grid grid-cols-2 place-items-center absolute w-screen top-0 pointer-events-none overflow-scroll"
->
-  <div
-    class={$modeCurrent
-      ? 'block card p-8 pointer-events-auto shadow-xl shadow-surface-300 sm:w-screen md:w-5/6 justify-center'
-      : 'block card p-8 pointer-events-auto shadow-xl shadow-surface-500 sm:w-screen md:w-5/6 justify-center'}
-  >
-    <div class="p-2 rounded-md">
-      <h2 class="h2">Hello {data.user?.firstName},</h2>
-      <br />
-      {#if (data.user?.membershipExpDate.getTime() ?? 0) < new Date().getTime()}
-        <h6 class="badge variant-filled-error">Looks like your dues are Expired!</h6>
-        <hr />
-        <br class="h-5" />
-        <PayDues bind:purchaseSuccess={paymentSuccess} />
-        <form method="post">
-          <input type="hidden" name="email" id="email" bind:value={email} />
-          <input type="hidden" name="duesType" id="duesType" bind:value={paymentSuccess.duesType} />
-          <button style="visibility: hidden;" type="submit" id="submitPaypal" />
-        </form>
-      {:else}
-        <h6 class="badge variant-filled-success">
-          Your Dues expire on {data.user?.membershipExpDate.toDateString()}
-        </h6>
-        <h6 class="h6">
-          Looks like you're all set! check back in on discord in about a day after paying dues for
-          membership status, and look out for announcements about updates to this site!
-        </h6>
-        <a
-          id="survey"
-          target="_blank"
-          href="https://docs.google.com/forms/d/e/1FAIpQLSc8G4hIVlv9rUusUO6Kb1eZZ-uGbS5TPp0Agi4LYsZZGoHkJQ/viewform?usp=sf_link"
-          class="btn variant-ghost-tertiary hover:variant-filled-tertiary">Member Survey</a
-        >
-      {/if}
-    </div>
-  </div>
-</div> -->
