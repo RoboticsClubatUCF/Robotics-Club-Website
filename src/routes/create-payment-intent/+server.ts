1b5bd456af74ab8fb6d5ea69a1f2360b11a24396
CaiCheng-Li <caicheng_li@yahoo.com>
2024-08-28 14:18:30 -0400

    Yearly Dues - not (fully) working

--- src/routes/create-payment-intent/+server.ts
+++ src/routes/create-payment-intent/+server.ts
@@ -1,34 +1,21 @@
 import Stripe from 'stripe';
 import { SECRET_STRIPE_KEY } from '$env/static/private';
 import config from '../../config';
-import { duesType } from '../../stores';
-import { get } from 'svelte/store';
 
 // initialize Stripe
 const stripe = new Stripe(SECRET_STRIPE_KEY);
-// handle POST /create-payment-intent
-export async function POST() {
-  // create the payment intent
+
+export async function POST({ request }) {
+  const { duesType } = await request.json();
+
+  const amount = duesType === '1' ? config.paypal.semester_cost : config.paypal.year_cost;
+  // console.log(amount);
+
   const paymentIntent = await stripe.paymentIntents.create({
-    amount: Number(config.paypal.semester_cost) * 100,
-    // note, for some EU-only payment methods it must be EUR
+    amount: Number(amount) * 100, // Stripe amounts are in cents
     currency: 'USD',
-    // specify what payment methods are allowed
-    // can be card, sepa_debit, ideal, etc...
     payment_method_types: ['card']
   });
 
-  // return the clientSecret to the client
   return new Response(JSON.stringify({ clientSecret: paymentIntent.client_secret }));
 }
-
-// "variable" is the variable configured in the stores.ts
-// function figureOutDues() {
-//   const ds = get(duesType);
-//   console.log(ds);
-//   if (ds == '1') {
-//     return Number(config.paypal.year_cost);
-//   } else {
-//     return Number(config.paypal.semester_cost);
-//   }
-// }
