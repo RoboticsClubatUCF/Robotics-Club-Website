3404a44be5795b61ece3444ce3c6e3f6f8a20011
BiggiePete <pter.cross222@gmail.com>
2024-09-09 11:58:08 -0400

    backup email load

--- src/routes/(app)/dashboard/survey/+page.server.ts
+++ src/routes/(app)/dashboard/survey/+page.server.ts
@@ -1,139 +1,147 @@
-import { z } from "zod";
-import type { Actions, PageServerLoad } from "./$types";
-import { setError, superValidate } from "sveltekit-superforms/server";
-import { fail, redirect } from "@sveltejs/kit";
-import { db } from "$lib/db";
+import { z } from 'zod';
+import type { Actions, PageServerLoad } from './$types';
+import { setError, superValidate } from 'sveltekit-superforms/server';
+import { fail, redirect } from '@sveltejs/kit';
+import { db } from '$lib/db';
 
 let userEmail = '';
-console.log("On Inital: " + userEmail);
+console.log('On Inital: ' + userEmail);
 const surSchema = z.object({
-    // answers to questions here
-    gitName: z.string(),
-    ucfEmail: z.string().email().refine(email => email.endsWith('@ucf.edu'), {
-        message: "Email must be a valid UCF email address (@ucf.edu)"
+  // answers to questions here
+  gitName: z.string(),
+  ucfEmail: z
+    .string()
+    .email()
+    .refine((email) => email.endsWith('@ucf.edu'), {
+      message: 'Email must be a valid UCF email address (@ucf.edu)'
     }),
-    Major: z.string().array(),
-    oMajor: z.string().optional(),
-    year: z.string(),
-    semester: z.number(),
-    shirtSize: z.string(),
-    prevMem: z.string(),
-    discover: z.string().array(),
-    allergies: z.string().array(),
-    oAllergies: z.string().optional(),
-    otherConcerns: z.string().optional()
+  Major: z.string().array(),
+  //   exmaple on moving error messages into zod object
+  //   Major: z.string().array().min(1,"At least one of the options must be selected"),
+  oMajor: z.string().optional(),
+  year: z.string(),
+  semester: z.number(),
+  shirtSize: z.string(),
+  prevMem: z.string(),
+  discover: z.string().array(),
+  allergies: z.string().array(),
+  oAllergies: z.string().optional(),
+  otherConcerns: z.string().optional()
 });
 
-export const load: PageServerLoad = async ({ locals }) => {
-    userEmail = locals.member!.email;
+export const load: PageServerLoad = async ({ locals, parent }) => {
+  userEmail = locals.member?.email ?? (await parent()).member.email;
 
-    // Check if user already has a survey
-    const existingSurvey = await db.survey.findFirst({
-        where: {
-            Member: {
-                some: {
-                    email: userEmail
-                }
-            }
+  // Check if user already has a survey
+  const existingSurvey = await db.survey.findFirst({
+    where: {
+      Member: {
+        some: {
+          email: userEmail
         }
-    });
-
-    if (existingSurvey) {
-        throw redirect(302, '/dashboard');
-    }else{
-        console.log("On load userEmail: " + userEmail);
+      }
     }
+  });
 
-    const form = await superValidate(surSchema);
-    return { form };
+  if (existingSurvey) {
+    throw redirect(302, '/dashboard');
+  } else {
+    console.log('On load userEmail: ' + userEmail);
+  }
+
+  const form = await superValidate(surSchema);
+  return { form };
 };
 
 export const actions: Actions = {
-    default: async({ request }) => {
-        const form = await superValidate(request, surSchema);
+  default: async ({ request }) => {
+    const form = await superValidate(request, surSchema);
 
-        // Validating forms
-        if (!form.valid) {
-            return fail(400, { form });
-        }
-
-        // Check if the user already has a survey to handle race condition
-        if (await db.survey.findFirst({
-            where: {
-                UCFemail: form.data.ucfEmail
-            }
-        })) {
-            return setError(form, 'ucfEmail', 'Email is already being used!');
-        }
-
-        const selectedMajors = form.data.Major.filter(major => major !== '');
-        const selectedyear = form.data.year;
-        const selectedshirtSize = form.data.shirtSize;
-        const selectedprevMem = form.data.prevMem;
-        const selectedallergies = form.data.allergies.filter(allergies => allergies !== '');
-        const enteredNum = form.data.semester;
-        const selectedDiscover = form.data.discover.filter(allergies => allergies !== '');
-
-        if (selectedMajors.length === 0) {
-            return setError(form, 'Major', 'At least one of the options must be selected');
-        }
-        if (selectedMajors.includes("Other") && !form.data.oMajor) {
-            return setError(form, 'oMajor', 'Please enter Major');
-        }
-        if (selectedyear === '') {
-            return setError(form, 'year', 'At least one of the options must be selected');
-        }
-        if (selectedshirtSize === '') {
-            return setError(form, 'shirtSize', 'At least one of the options must be selected');
-        }
-        if (selectedprevMem === '') {
-            return setError(form, 'prevMem', 'At least one of the options must be selected');
-        }
-        if (selectedDiscover.length === 0) {
-            return setError(form, 'discover', 'At least one of the options must be selected');
-        }
-        if (enteredNum < 1 && selectedprevMem === 'Yes'){
-            return setError(form, 'semester', 'Please enter a number >= 0');
-        }
-        if (enteredNum > 30){
-            return setError(form, 'semester', 'Really?');
-        }
-        if (selectedallergies.length === 0) {
-            return setError(form, 'allergies', 'At least one of the options must be selected');
-        }
-        // New validation for allergies: "None" should not be selected with other allergies
-        if (selectedallergies.includes("None") && selectedallergies.length > 1) {
-            return setError(form, 'allergies', 'Connot have both None and allergen(s) selected');
-        }
-        if (selectedallergies.includes("Other") && !form.data.oAllergies) {
-            return setError(form, 'oAllergies', 'Please enter Allergen(s)');
-        }
-
-        console.log("On create userEmail: " + userEmail);
-
-        // Creating survey entry in the database
-        await db.survey.create({
-            data: {
-                GitName: form.data.gitName,
-                UCFemail: form.data.ucfEmail,
-                Major: form.data.Major,
-                OtherMajors: form.data.oMajor,
-                Year: form.data.year,
-                NumberofSemesters: form.data.semester,
-                ShirtSize: form.data.shirtSize,
-                PrevMem: form.data.prevMem,
-                DiscoveredThrough: form.data.discover,
-                Allergies: form.data.allergies,
-                OtherAllergies: form.data.oAllergies,
-                Concerns: form.data.otherConcerns,
-                Member: {
-                    connect: {
-                        email: userEmail
-                    }
-                }
-            }
-        });
-
-        throw redirect(302, '/dashboard');
+    // Validating forms
+    if (!form.valid) {
+      return fail(400, { form });
     }
+
+    // Check if the user already has a survey to handle race condition
+    if (
+      await db.survey.findFirst({
+        where: {
+          UCFemail: form.data.ucfEmail
+        }
+      })
+    ) {
+      return setError(form, 'ucfEmail', 'Email is already being used!');
+    }
+
+    const selectedMajors = form.data.Major.filter((major) => major !== '');
+    const selectedyear = form.data.year;
+    const selectedshirtSize = form.data.shirtSize;
+    const selectedprevMem = form.data.prevMem;
+    const selectedallergies = form.data.allergies.filter((allergies) => allergies !== '');
+    const enteredNum = form.data.semester;
+    const selectedDiscover = form.data.discover.filter((allergies) => allergies !== '');
+
+    // please move all of these errors into the ZOD object, so when the form is validated, all of these steps are taken into account
+    if (selectedMajors.length === 0) {
+      return setError(form, 'Major', 'At least one of the options must be selected');
+    }
+    if (selectedMajors.includes('Other') && !form.data.oMajor) {
+      return setError(form, 'oMajor', 'Please enter Major');
+    }
+    if (selectedyear === '') {
+      return setError(form, 'year', 'At least one of the options must be selected');
+    }
+    if (selectedshirtSize === '') {
+      return setError(form, 'shirtSize', 'At least one of the options must be selected');
+    }
+    if (selectedprevMem === '') {
+      return setError(form, 'prevMem', 'At least one of the options must be selected');
+    }
+    if (selectedDiscover.length === 0) {
+      return setError(form, 'discover', 'At least one of the options must be selected');
+    }
+    if (enteredNum < 1 && selectedprevMem === 'Yes') {
+      return setError(form, 'semester', 'Please enter a number >= 0');
+    }
+    if (enteredNum > 30) {
+      return setError(form, 'semester', 'Really?');
+    }
+    if (selectedallergies.length === 0) {
+      return setError(form, 'allergies', 'At least one of the options must be selected');
+    }
+    // New validation for allergies: "None" should not be selected with other allergies
+    if (selectedallergies.includes('None') && selectedallergies.length > 1) {
+      return setError(form, 'allergies', 'Connot have both None and allergen(s) selected');
+    }
+    if (selectedallergies.includes('Other') && !form.data.oAllergies) {
+      return setError(form, 'oAllergies', 'Please enter Allergen(s)');
+    }
+
+    console.log('On create userEmail: ' + userEmail);
+
+    // Creating survey entry in the database
+    await db.survey.create({
+      data: {
+        GitName: form.data.gitName,
+        UCFemail: form.data.ucfEmail,
+        Major: form.data.Major,
+        OtherMajors: form.data.oMajor,
+        Year: form.data.year,
+        NumberofSemesters: form.data.semester,
+        ShirtSize: form.data.shirtSize,
+        PrevMem: form.data.prevMem,
+        DiscoveredThrough: form.data.discover,
+        Allergies: form.data.allergies,
+        OtherAllergies: form.data.oAllergies,
+        Concerns: form.data.otherConcerns,
+        Member: {
+          connect: {
+            email: userEmail
+          }
+        }
+      }
+    });
+
+    throw redirect(302, '/dashboard');
+  }
 };
