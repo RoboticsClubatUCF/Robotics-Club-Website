29750f69e74b27789905b2d9c07ec636578873ca
CaiCheng-Li <caicheng_li@yahoo.com>
2024-10-24 14:55:16 -0400

    Sponsors page Update

--- src/routes/(main)/sponsors/+page.svelte
+++ src/routes/(main)/sponsors/+page.svelte
@@ -1,100 +1,27 @@
 <script>
-
-</script>
-
-<style>
-    .centered {
-        max-width: 100vw; /* Can be in percentage also. */
-        height: auto;
-        margin: 0 auto;
-        margin-top: 3rem;
-        padding: 10px;
-        margin-bottom: 2rem;
-        position: relative;
-        text-align: center;
-        animation: fadeIn 3s;
+    // Image link to load
+    let imageUrl = "https://lh3.googleusercontent.com/pw/AP1GczPgwQIAh4n8CAPbGOEQod39G73J6kmg1OxO5RKxnOEdcULy52h5DdDLxmE9vTKLfO7tYJGCQIV-E7TramY7eH_VvlgA1nX4MdcN5zJanqqBzauTUsY=w2400";
+  </script>
+  
+  <style>
+    img {
+      max-width: 100%;
+      height: auto;
+      display: block;
+      margin: 0 auto;
     }
-</style>
+  </style>
+  
+  <main>
+    <!-- svelte-ignore a11y-img-redundant-alt -->
+    <img src={imageUrl} alt="Image description" />
+    <!-- svelte-ignore a11y-img-redundant-alt -->
+    <img src={"https://lh3.googleusercontent.com/pw/AP1GczPo2li6sb8MSLo-TzvYzTaYorf_o8ogywvmAE3uo3bsTTK_UHZI7PFhDbZUrJnv1AhN0xVARAPEEg4Z_UbBigzPYObemFC9OPMfLJs6VzK71FlbLn0=w2400"} alt="Image description" />
+    <!-- svelte-ignore a11y-img-redundant-alt -->
+    <img src={"https://lh3.googleusercontent.com/pw/AP1GczNUJ2rVGh-LyeWqjex65MdtxVo6-HJ_kQs1kfIJwbbKoyd0t6HqrxvCVa6xTo2OX53ZrA2jl9iU2nspKhKp_2LvGH9kYztHstPrz-iBHl_PnWtrHc8=w2400"} alt="Image description" />
+     <h1><br></h1>
+    <!-- svelte-ignore a11y-img-redundant-alt -->
+    <img src={"https://lh3.googleusercontent.com/pw/AP1GczMi_pH21kxBUCqEuqio8A-tphN2IhuVgJ5EfjtL_RwNLfs8ln1dyg06d5I0kRWASOAtPJgpZ8T5jR-G29sjqY7wHfRmJE2E2_0UC_MzMvm3AaG1eSI=w2400"} alt="Image description" />
 
-<div class="w-screen flex">
-    <div class="centered h1">
-        Our Sponsors!
-        <div class="h3">
-            coming soon...
-        </div>
-    </div>
-</div>
-
-<!-- for when we have sponsors ;( -->
-<div class="m-4" style="margin-top: 2rem">
-    <section class="grid md:grid-cols-4 gap-4">
-        <!-- <div class="grid gap-4">
-            <div>
-                <img class="h-auto max-w-full rounded-lg" src="https://lh3.googleusercontent.com/pw/AP1GczPyeQHO1GXHPo3X5mFtnVgCCd-N98001LLTqYU8xm7pc3xl-lN8Uo36ZiKJKdUmgil-xtV0aUL6RN2fnRPTNyGnMTToWZ2ssjBHYmrjrmTEdaw8QwEqKI6unTlmhH-KmTGTqunjnX5COZ3Vkh2Eu2o=w958-h721-s-no-gm?authuser=0" alt=""/>
-            </div>
-            <div>
-                <img class="h-auto max-w-full rounded-lg" src="https://lh3.googleusercontent.com/pw/AP1GczN8IIEMyGOYPR9etkhdi-xj4KtyK4joyK5p4xUfRoasP2oCf_PsMQ89x6C2pAuZAgsj5SMKmNauyNtgvy9PNVeFw_8v-ariktVlpG5dOBT82x48-vo=w1920-h1080" alt="" />
-            </div>
-            <div>
-                <img class="h-auto max-w-full rounded-lg" src="https://lh3.googleusercontent.com/pw/AP1GczOmD8rKdjyLqFF0RSR8-BW2JsKLT9cVgx7XA1DR3TKP-YmD3HMfmww2Q2jTM1cZctZ0TIzQ4nOM2r-ixBZXDCBgrM0jc8V3EgYtwfg0RQfqVbc5kMX0isCDCJNHXduUwHPoEt_U3xW55O7TsiUC2JL4=w629-h944-s-no-gm?authuser=0" alt="" />
-            </div>
-        </div>
-        <div class="grid gap-4">
-            <div class="grid gap-4">
-                <div>
-                    <img class="h-auto max-w-full rounded-lg" src="https://lh3.googleusercontent.com/pw/AP1GczPyeQHO1GXHPo3X5mFtnVgCCd-N98001LLTqYU8xm7pc3xl-lN8Uo36ZiKJKdUmgil-xtV0aUL6RN2fnRPTNyGnMTToWZ2ssjBHYmrjrmTEdaw8QwEqKI6unTlmhH-KmTGTqunjnX5COZ3Vkh2Eu2o=w958-h721-s-no-gm?authuser=0" alt=""/>
-                </div>
-                <div>
-                    <img class="h-auto max-w-full rounded-lg" src="https://lh3.googleusercontent.com/pw/AP1GczN8IIEMyGOYPR9etkhdi-xj4KtyK4joyK5p4xUfRoasP2oCf_PsMQ89x6C2pAuZAgsj5SMKmNauyNtgvy9PNVeFw_8v-ariktVlpG5dOBT82x48-vo=w1920-h1080" alt="" />
-                </div>
-                <div>
-                    <img class="h-auto max-w-full rounded-lg" src="https://lh3.googleusercontent.com/pw/AP1GczOmD8rKdjyLqFF0RSR8-BW2JsKLT9cVgx7XA1DR3TKP-YmD3HMfmww2Q2jTM1cZctZ0TIzQ4nOM2r-ixBZXDCBgrM0jc8V3EgYtwfg0RQfqVbc5kMX0isCDCJNHXduUwHPoEt_U3xW55O7TsiUC2JL4=w629-h944-s-no-gm?authuser=0" alt="" />
-                </div>
-            </div>
-        </div>
-        <div class="grid gap-4">
-            <div class="grid gap-4">
-                <div>
-                    <img class="h-auto max-w-full rounded-lg" src="https://lh3.googleusercontent.com/pw/AP1GczPyeQHO1GXHPo3X5mFtnVgCCd-N98001LLTqYU8xm7pc3xl-lN8Uo36ZiKJKdUmgil-xtV0aUL6RN2fnRPTNyGnMTToWZ2ssjBHYmrjrmTEdaw8QwEqKI6unTlmhH-KmTGTqunjnX5COZ3Vkh2Eu2o=w958-h721-s-no-gm?authuser=0" alt=""/>
-                </div>
-                <div>
-                    <img class="h-auto max-w-full rounded-lg" src="https://lh3.googleusercontent.com/pw/AP1GczN8IIEMyGOYPR9etkhdi-xj4KtyK4joyK5p4xUfRoasP2oCf_PsMQ89x6C2pAuZAgsj5SMKmNauyNtgvy9PNVeFw_8v-ariktVlpG5dOBT82x48-vo=w1920-h1080" alt="" />
-                </div>
-                <div>
-                    <img class="h-auto max-w-full rounded-lg" src="https://lh3.googleusercontent.com/pw/AP1GczOmD8rKdjyLqFF0RSR8-BW2JsKLT9cVgx7XA1DR3TKP-YmD3HMfmww2Q2jTM1cZctZ0TIzQ4nOM2r-ixBZXDCBgrM0jc8V3EgYtwfg0RQfqVbc5kMX0isCDCJNHXduUwHPoEt_U3xW55O7TsiUC2JL4=w629-h944-s-no-gm?authuser=0" alt="" />
-                </div>
-            </div>
-        </div>
-        <div class="grid gap-4">
-            <div class="grid gap-4">
-                <div>
-                    <img class="h-auto max-w-full rounded-lg" src="https://lh3.googleusercontent.com/pw/AP1GczPyeQHO1GXHPo3X5mFtnVgCCd-N98001LLTqYU8xm7pc3xl-lN8Uo36ZiKJKdUmgil-xtV0aUL6RN2fnRPTNyGnMTToWZ2ssjBHYmrjrmTEdaw8QwEqKI6unTlmhH-KmTGTqunjnX5COZ3Vkh2Eu2o=w958-h721-s-no-gm?authuser=0" alt=""/>
-                </div>
-                <div>
-                    <img class="h-auto max-w-full rounded-lg" src="https://lh3.googleusercontent.com/pw/AP1GczOmD8rKdjyLqFF0RSR8-BW2JsKLT9cVgx7XA1DR3TKP-YmD3HMfmww2Q2jTM1cZctZ0TIzQ4nOM2r-ixBZXDCBgrM0jc8V3EgYtwfg0RQfqVbc5kMX0isCDCJNHXduUwHPoEt_U3xW55O7TsiUC2JL4=w629-h944-s-no-gm?authuser=0" alt="" />
-                </div>
-                    <img class="h-auto max-w-full rounded-lg" src="https://lh3.googleusercontent.com/pw/AP1GczN8IIEMyGOYPR9etkhdi-xj4KtyK4joyK5p4xUfRoasP2oCf_PsMQ89x6C2pAuZAgsj5SMKmNauyNtgvy9PNVeFw_8v-ariktVlpG5dOBT82x48-vo=w1920-h1080" alt="" />
-                <div>
-                </div>
-            </div>
-        </div> -->
-    </section>
-</div>
-
-<div class="w-screen flex">
-    <div class="centered h1">
-        Benefits!
-        <div class="h3">
-            coming soon...
-        </div>
-    </div>
-</div>
-
-<div class="w-screen flex">
-    <div class="centered h1">
-        Becoming a Sponsor!
-        <div class="h3">
-            coming soon...
-        </div>
-    </div>
-</div>
+  </main>
+  
