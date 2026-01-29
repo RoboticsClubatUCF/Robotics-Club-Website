1a271438a79599e09e6e84bbf9fdb071d02da0cc
CaiCheng-Li <caicheng_li@yahoo.com>
2026-01-28 23:11:51 -0500

    New Officers

--- src/components/cards/admins.svelte
+++ src/components/cards/admins.svelte
@@ -17,53 +17,53 @@
     </div>
   </a>
 
-  <a class="card card-hover overflow-hidden" href="https://www.linkedin.com/in/alexander-alen-b82948265/">
+  <a class="card card-hover overflow-hidden" href="https://www.linkedin.com/in/fitzgeraldgavin">
     <img
-      src="https://lh3.googleusercontent.com/pw/AP1GczMT20J0cAADEs2FwQROOoejetahZZ1PnSkfzjtrUL8byIUZ8kD_4PG-r0KocTeNpRZsemwsrrrtVc4AY1IJZgXB5PRGiNxlGt6IMZ1EB-iTxwdl1x3j2_G2SaGwDPlSL-Xvb5amiRkAH_S2szHMVsc=w607-h910-s-no-gm?authuser=0"
+      src="/photos/President.jpg"
       class="bg-black/50 w-full aspect-[10/8] object-cover"
       alt="Post"
     />
     <div class="p-4 space-y-4">
-      <h6 class="h3">Alexander Alen - President</h6>
+      <h6 class="h3">Gavin Fitzgerald - President</h6>
     </div>
   </a>
 
-  <a class="card card-hover overflow-hidden" href="https://www.linkedin.com/in/gavin0fitzgerald/">
+  <a class="card card-hover overflow-hidden" href="/">
     <img
-      src="https://lh3.googleusercontent.com/pw/AP1GczN5plp4-Zo5mIPiISB2-bBSKCGOH8F9zhqinHQaJQGgX9KZ2juYotImCyGI5EtDarzsl1c3qPazsg3D-U-d2DjQlGiPTNshQfTFChgv-fpJx5Qs1_M=w2400"
+      src="/photos/VP.jpg"
       class="bg-black/50 w-full aspect-[10/8] object-cover"
       alt="Post"
     />
     <div class="p-4 space-y-4">
-      <h6 class="h3">Gavin Fitzgerald - Vice President</h6>
+      <h6 class="h3">Stephen Chapman - Vice President</h6>
     </div>
   </a>
 
-  <a class="card card-hover overflow-hidden" href="https://www.linkedin.com/in/olivia-mueller-8977a7250/">
+  <a class="card card-hover overflow-hidden" href="https://www.linkedin.com/in/mauriceelkhoury">
     <img
-      src="https://lh3.googleusercontent.com/pw/AP1GczOLw8ih72GuV4ptzsdD6E96TSRkEJWrHnHe_ZoNlOaSfwjkS4rS7mhgJGlintHEYEpYJXnWzl6YXhofAmx8VvuNm8pllcRSHA_x6YkChIOkjhmTUgk=w2400"
+      src="/photos/Treasure.jpg"
       class="bg-black/50 w-full aspect-[10/8] object-cover"
       alt="Post"
     />
     <div class="p-4 space-y-4">
-      <h6 class="h3">Ollie Mueller - Treasurer</h6>
+      <h6 class="h3">Maurice Elkhoury - Treasurer</h6>
     </div>
   </a>
   
-  <a class="card card-hover overflow-hidden" href="https://www.linkedin.com/in/maurice-elkhoury-065b63326/">
+  <a class="card card-hover overflow-hidden" href="/">
     <img
-      src="https://lh3.googleusercontent.com/pw/AP1GczNz0JRi4hSGGnngv7CH0_aFPqDxm_xoCcuLsbCos7G2fFIqz7vG3qOaAtyCezDtohjpuO941KiDGCcbP-_0u_hxYxtr0tJxQFNlBYHn8uvDMsfB5tk=w2400"
+      src="/photos/Secretary.jpg"
       class="bg-black/50 w-full aspect-[10/8] object-cover"
       alt="Post"
     />
     <div class="p-4 space-y-4">
-      <h6 class="h3">Maurice Elkhoury - Secretary</h6>
+      <h6 class="h3">Aiden Fowler - Secretary</h6>
     </div>
   </a>
 
-  <a class="card card-hover overflow-hidden" href="https://www.linkedin.com/in/zahidpadilla/">
+  <a class="card card-hover overflow-hidden" href="https://www.instagram.com/blank1pro/">
     <img
-      src="https://lh3.googleusercontent.com/pw/AP1GczMf0Ky7DMbPVNG17KL8MNEKqwtciUteO3QW-6CC4oRtoRW-i7BCOtvr59iFqMnqxomhLoA04a8WytTjMLAyQAId6misoR3GBwSryTUklFncR0Q_oQ0=w2400"
+      src="/photos/Outreach.jpg"
       class="bg-black/50 w-full aspect-[10/8] object-cover"
       alt="Post"
     />
@@ -72,20 +72,20 @@
     </div>
   </a>
 
-  <a class="card card-hover overflow-hidden" href="https://www.linkedin.com/in/sofiado2210/">
+  <a class="card card-hover overflow-hidden" href="https://www.linkedin.com/in/lily-rieckhoff-771a48270?trk=public_post-text">
     <img
-      src="https://lh3.googleusercontent.com/pw/AP1GczPVYnO-DDXhsT2tT3PZ7AUxGh9_ZJopR4N7QyhvqGoIqQ6SW5oPOROIDcNmo5erXzuP6VFPlVEYFNmwKoPfBAgswvzLJsK2-pf5MFunRZ8d4_cOBRwClyYEEQYvIYWYEmeGlux5STk5RlGyEhc4rHA=w607-h911-s-no-gm?authuser=0"
+      src="/photos/Marketing.jpg"
       class="bg-black/50 w-full aspect-[10/8] object-cover"
       alt="Post"
     />
     <div class="p-4 space-y-4">
-      <h6 class="h3">Sofia Delgado Ordaz - Head of Marketing</h6>
+      <h6 class="h3">Lily Rieckhoff - Head of Marketing</h6>
     </div>
   </a>
 
   <a class="card card-hover overflow-hidden" href="https://www.linkedin.com/in/caicheng-li-1659a3300/">
     <img
-      src="https://lh3.googleusercontent.com/pw/AP1GczNAKCXZvNHerYZ-M2Avq8pMooGh55HOLMeidwAfi0wfO1j3J1eNqs7x6EwrlcUAnQFlMzKZjTNlruHyaZhsDbspzNxZ9aHrG2QXJ_5Ik0srjenRZ58=w2400"
+      src="/photos/Manager.jpg"
       class="bg-black/50 w-full aspect-[10/8] object-cover"
       alt="Post"
     />
