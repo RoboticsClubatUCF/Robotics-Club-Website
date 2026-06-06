7f1ef2ba454f5e0d58fbb972b6c5e809c7ceff18
CaiCheng-Li <caicheng_li@yahoo.com>
2026-06-05 23:46:24 -0400

    cloudflare fix?

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,17 @@
 <script lang="ts">
   import { Toast, Modal, initializeStores } from '@skeletonlabs/skeleton';
+  import { beforeNavigate } from '$app/navigation';
+
   initializeStores();
+
+  const CHALLENGED_ROUTES = ['/register', '/forgotPass'];
+
+  beforeNavigate(({ to, cancel }) => {
+    if (to && CHALLENGED_ROUTES.some((r) => to.url.pathname.startsWith(r))) {
+      cancel();
+      window.location.href = to.url.href;
+    }
+  });
 </script>
 
 <Modal buttonPositive="variant-filled-error" />
